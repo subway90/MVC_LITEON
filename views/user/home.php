ebeb2b616d6ae9c35d1799f8faa3f7e49a2b66b5
subway90 <nguyenhieu3105@gmail.com>
2025-05-16 19:07:41 +0700

    test deploy

--- views/user/home.php
+++ views/user/home.php
@@ -2,7 +2,7 @@
     <div class="col-12 col-md-8 col-lg-6">
         <form method="post" action="/check" class="d-flex flex-column align-items-center justify-content-center mt-5">
             <label for="name" class="text-yellow fw-bold fs-5 text-center mb-2 text-uppercase">
-                TEST <br>
+                Vui lòng nhập họ và tên quý khách (Có dấu) <br>
                 請輸入您的姓名 (PASSPORT/VIETNAMESE NAME)
             </label>
             <div class="col-12 col-md-8 col-lg-6">
